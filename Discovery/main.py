9610f6fe7356fca4c1ce35c47300201573b70a5b
encodedemotions <57257684+encodedemotions@users.noreply.github.com>
2022-10-04 19:41:05 +0300

    Add progress

--- Discovery/main.py
+++ Discovery/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 import requests
 addressList = ['http://127.0.0.1:7001/']
 app = Flask(__name__)
@@ -7,9 +7,18 @@ app = Flask(__name__)
 def main():
     data = {}
     for address in addressList:
+        #make a post request and save the response
         response = requests.post(address)
         data[address] = response.json()
     return jsonify(data)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    currentRequest = request
+    remote_addr = currentRequest.remote_addr
+    port = currentRequest.json()["port"]
+
+    print(currentRequest)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=6969)
