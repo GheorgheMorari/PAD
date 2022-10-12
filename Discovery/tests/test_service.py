c99d1fdddc9160f7441ebcc7de766be965482003
Gheorghe Morari <53918731+GheorgheMorari@users.noreply.github.com>
2022-10-12 14:22:17 +0300

    rename discovery names

---
+++ Discovery/tests/test_service.py
@@ -0,0 +1,26 @@
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+port = 7001
+host = "127.0.0.1"
+discovery_service_address = "http://127.0.0.1:6969/"
+
+data = {
+    "ServiceName": "testService",
+    "Port": str(port),
+    "Host": host
+}
+
+
+@app.route('/status', methods=['GET', 'POST'])
+def main():
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    response = requests.post(discovery_service_address + "register", json=data)
+    if response.status_code != 200:
+        raise Exception("Request error")
+    app.run(host=host, port=port)
