ff2ad20b41b3f73793cb95dbb6727e04f0524536
encodedemotions <1botezatroman@gmail.com>
2022-10-26 17:05:29 +0300

    Add toxicity detection

---
+++ ToxicityDetectionService/toxicity_detection_adapter.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+
+TOXICITY_DETECTION_MODEL_NAME = "unitary/toxic-bert"
+TOXICITY_DETECTION_MODEL = pipeline('text-classification', model=TOXICITY_DETECTION_MODEL_NAME,
+                                    tokenizer=TOXICITY_DETECTION_MODEL_NAME)
+
+
+def predict_toxicity(text: str) -> dict:
+    results_list = TOXICITY_DETECTION_MODEL({"text": text}, top_k=6)
+    return results_list
