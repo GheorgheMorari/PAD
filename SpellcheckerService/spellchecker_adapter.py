7d8b40f50388113bcb56fa24be4346e779c6fe6b
encodedemotions <1botezatroman@gmail.com>
2022-10-26 16:20:07 +0300

    Add spellchecker

---
+++ SpellcheckerService/spellchecker_adapter.py
@@ -0,0 +1,24 @@
+import re
+
+from spellchecker import SpellChecker
+
+SPELLCHECKER_INSTANCE = SpellChecker()
+
+
+def predict(text: str) -> dict:
+    tokens = list(filter(None, re.split(r"[\s.,!?:;-]+", text)))
+    unique_tokens = set(tokens)
+    candidates_list = [(SPELLCHECKER_INSTANCE.candidates(token), token) for token in tokens]
+    misspelled = set()
+    corrected_content = text.lower()
+
+    for candidates, token in candidates_list:
+        if candidates is not None and token not in candidates:
+            misspelled.add(token)
+            corrected_content = corrected_content.replace(token, candidates.pop())
+
+    return {"misspelled_tokens": misspelled,
+            "corrected_content": corrected_content,
+            "misspelled_rate": (len(misspelled) / len(unique_tokens)) if len(
+                unique_tokens) != 0 else 0,
+            "misspelled_token_count": len(misspelled)}
---
+++ SpellcheckerService/spellchecker_main.py
@@ -0,0 +1,38 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from DiscoveryServiceUtils.discovery_comm import DiscoveryServiceComm
+from SpellcheckerService.spellchecker_adapter import predict
+
+app = FastAPI()
+SPELLCHECKER_SERVICE_HOST = "127.0.0.1"
+SPELLCHECKER_SERVICE_PORT = 8082
+SPELLCHECKER_SERVICE_NAME = "SpellcheckerService"
+SPELLCHECKER_DISCOVERY = DiscoveryServiceComm(service_name=SPELLCHECKER_SERVICE_NAME,
+                                              port=str(SPELLCHECKER_SERVICE_PORT))
+
+
+class Message(BaseModel):
+    text: str
+
+
+@app.post("/")
+def main(message: Message):
+    return predict(message.text)
+
+
+@app.post("/status")
+def status():
+    return SPELLCHECKER_DISCOVERY.get_status_data()
+
+
+if __name__ == "__main__":
+    if not SPELLCHECKER_DISCOVERY.check_connection():
+        raise Exception("Discovery service unavailable")
+
+    if not SPELLCHECKER_DISCOVERY.register_force():
+        raise Exception(f"Registration unsuccessful, Status code:{SPELLCHECKER_DISCOVERY.get_status_code()}")
+    print("Registration successful")
+
+    uvicorn.run(app, host=SPELLCHECKER_SERVICE_HOST, port=SPELLCHECKER_SERVICE_PORT)
