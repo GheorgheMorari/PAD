17f6d861c24aa5717ebc045336df9edd611d3bac
encodedemotions <1botezatroman@gmail.com>
2022-12-08 23:39:08 +0200

    Add DistributedServiceUtils package

---
+++ DistributedServiceUtils/distributed_service_comm.py
@@ -0,0 +1,36 @@
+import random
+from enum import Enum
+from typing import List, Optional
+
+import requests
+
+
+class AccessType(Enum):
+    round_robin = "round_robin"
+    choose_one = "choose_one"
+
+
+class DistributedServiceComm:
+    def __init__(self, service_address_list: List[str], access_type: AccessType = AccessType.round_robin):
+        self.service_address_list = service_address_list
+        self.access_type = access_type
+        self.address_counter = 0
+
+        self.reset_counter()
+
+    def reset_counter(self):
+        self.address_counter = random.randint(0, len(self.service_address_list))
+
+    def send_post(self, json_data: Optional[dict] = None, entrypoint="",
+                  address_index: Optional[int] = None) -> requests.Response:
+        if json_data is None:
+            json_data = dict()
+
+        if address_index:
+            return requests.post(self.service_address_list[address_index] + entrypoint, json=json_data)
+
+        response = requests.post(self.service_address_list[self.address_counter] + entrypoint, json=json_data)
+        if self.access_type.round_robin:
+            self.address_counter += 1
+            self.address_counter %= len(self.service_address_list)
+        return response
